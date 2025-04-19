b1fa7facd84d2dcc00b25bb54fad5c553ba301ae
611de <309255056@qq.com>
2025-04-19 22:52:48 +0800

    feat: add <Leader>+v desc in which_key

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -78,6 +78,7 @@ return {
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
         -- ["<Leader>b"] = { desc = "Buffers" },
+        ["<Leader>v"] = { desc = "PyVenv Selector" },
 
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
